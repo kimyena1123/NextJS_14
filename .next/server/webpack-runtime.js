9c81c6a70c4ac0475bb1996ddcf5ef2c17928619
kimyena1123 <kyq104@gmail.com>
2024-03-16 18:58:44 +0900

    promise setTImeout 사용

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("928e73c23e07e571")
+/******/ 		__webpack_require__.h = () => ("db125dd30b5ac50e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
